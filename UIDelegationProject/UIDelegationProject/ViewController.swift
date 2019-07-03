76ccc5fb0d7471dd1ec9b31e3a6e9d3cd9b8db6a
Joshua Hollandsworth <josh@joshholl.com>
2019-07-02 21:51:25 -0500

    added ui for uidelegation project

---
+++ UIDelegationProject/UIDelegationProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDelegationProject
+//
+//  Created by Josh Hollandsworth on 7/2/19.
+//  Copyright © 2019 Joshholl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
