f5d5184acdf3cb4c4de4ae39cbf8e3c0717a0249
Joshua Hollandsworth <josh@joshholl.com>
2019-07-07 22:19:27 -0500

    completing delegation project

--- UIDelegationProject/UIDelegationProject/AndroidHatersModel.swift
+++ UIDelegationProject/UIDelegationProject/AndroidHatersModel.swift
@@ -8,64 +8,127 @@
 
 import Foundation
 
-protocol AndroidHatersDelegate: class {
-    func onSaved(haterInfo: String)
-}
-
-struct AndroidHater {
-    let name: String
-    let phoneNumber: String
-    let emailAddress: String
-    let username: String
-    let password: String
-}
-
-
-public class AndroidHatersModel {
+struct AndroidHaterModel {
+    var name: String
+    var phoneNumber: String
+    var emailAddress: String
+    var username: String
+    var password: String
     
-    private weak var delegate: AndroidHatersDelegate?;
-    
-    init(delegate: AndroidHatersDelegate) {
-       self.delegate = delegate
+    var displayable: String {
+        return "🍏 Name: \(name), phone \(phoneNumber), email \(emailAddress), username \(username), password \(password)"
     }
-    
-    private var haters: [AndroidHater] = []
-    
-    func addHater(hater: AndroidHater) {
-        haters.append(hater)
-    }
-
-    func isValidName(text: String?) -> Bool {
-        return false
-    }
-    
-    func isValidPhoneNumber(text: String?) -> Bool {
-        guard let value = text else {
+   
+    private static func hasMiniumLength(text: String?, isAtLeast minLength: Int) -> Bool {
+        guard let target = text else {
             return false
         }
-        return matchesPattern(text: value, patter: "[0-9]{3}")
-    
+        return target.count >= minLength
+    }
+    static func isValidName(text: String?) -> Bool {
+        return hasMiniumLength(text: text, isAtLeast: 3)
     }
     
-    func isValidEmailAddress(text: String?) -> Bool {
-        return false
-    }
-    
-    func isValidUsername(text: String?) -> Bool {
-        return false
-    }
-    
-    func isValidPassword(text: String?) -> Bool {
-        return false
-    }
-    
-    private func matchesPattern(text: String, patter: String) -> Bool {
-        let result = text.range(of: patter, options: .regularExpression)
-        guard let matches = result else {
+    static func isValidPhoneNumber(text: String?) -> Bool {
+        guard let value = text, let matches = value.range(of: "^[0-9]{10}$", options: .regularExpression) else {
             return false
         }
         return !matches.isEmpty
+        
+    }
+    
+    static func isValidEmailAddress(text: String?) -> Bool {
+        return hasMiniumLength(text: text, isAtLeast: 3) && text!.contains("@")
+    }
+    
+    static func isValidUsername(text: String?) -> Bool {
+        return hasMiniumLength(text: text, isAtLeast: 3)
+    }
+    
+    static func isValidPassword(text: String?) -> Bool {
+        return hasMiniumLength(text: text, isAtLeast: 7)
+    }
+}
+
+
+
+
+protocol AndroidHatersDelegate: class {
+    func onSaved()
+    func onCanSave(canSave: Bool)
+}
+
+
+
+
+
+public class AndroidHatersModel {
+    private var haters: [AndroidHaterModel] = []
+    private weak var delegate: AndroidHatersDelegate!
+    
+    private func hasMiniumLength(target: String?, minimum: Int) -> Bool {
+        guard let value = target else {
+            return false
+        }
+        return value.count >= minimum
+    }
+    
+    var name: String? {
+        didSet {
+            checkIfCanSave()
+        }
+    }
+    var phoneNumber: String?{
+        didSet {
+            checkIfCanSave()
+        }
+    }
+    var email: String?{
+        didSet {
+            checkIfCanSave()
+        }
+    }
+    var username: String?{
+        didSet {
+            checkIfCanSave()
+        }
+    }
+    var password: String?{
+        didSet {
+            checkIfCanSave()
+        }
+    }
+    
+    init(delegate: AndroidHatersDelegate) {
+        self.delegate = delegate
     }
     
     
+    func addHater() {
+        haters.append(AndroidHaterModel(name: name!, phoneNumber: phoneNumber!, emailAddress: email!, username: username!, password: password!))
+        name = nil
+        phoneNumber = nil
+        email = nil
+        username = nil
+        password = nil
+        self.delegate.onSaved()
+    }
+    
+    func hatersAsString() -> String {
+         return haters.map{ $0.displayable }.joined(separator: "\n")
+    }
+    
+    private func checkIfCanSave() {
+        let canSave =  [name, email, phoneNumber, username, password].allSatisfy{ $0 != nil }
+        delegate.onCanSave(canSave: canSave)
+    }
+}
+
+
+
+extension AndroidHatersModel {
+    
+
+    
+   
 }
--- UIDelegationProject/UIDelegationProject/AndroidHatersViewController.swift
+++ UIDelegationProject/UIDelegationProject/AndroidHatersViewController.swift
@@ -1,81 +1,117 @@
-//
-//  ViewController.swift
-//  UIDelegationProject
-//
-//  Created by Josh Hollandsworth on 7/2/19.
-//  Copyright © 2019 Joshholl. All rights reserved.
-//
-
 import UIKit
 
 class AndroidHatersViewController: UIViewController {
-
+    
     @IBOutlet weak var nameTextField: UITextField!
-    
-    @IBOutlet weak var emailAddressTextFiled: UITextField!
-    
+    @IBOutlet weak var emailAddressTextField: UITextField!
     @IBOutlet weak var phoneNumberTextField: UITextField!
-    
     @IBOutlet weak var usernameTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var nameLabel: UILabel!
     
-    @IBOutlet weak var passwordLabel: UILabel!
-    @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var phoneNumberLabel: UILabel!
-    @IBOutlet weak var emailAddressLabel: UILabel!
-
+    @IBOutlet weak var haterTextField: UITextView!
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBAction func onJoinPressed(_ sender: Any) {
+        model.addHater()
+    }
+    
+    private var textFields: [UITextField] = []
     private var model: AndroidHatersModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         model = AndroidHatersModel(delegate: self)
+        
+        textFields = [nameTextField, emailAddressTextField, phoneNumberTextField, usernameTextField, passwordTextField ]
+        textFields.forEach{ $0.enablesReturnKeyAutomatically = true}
+        
+        
         nameTextField.delegate = self
-        emailAddressTextFiled.delegate = self
+        emailAddressTextField.delegate = self
         phoneNumberTextField.delegate = self
         usernameTextField.delegate = self
         passwordTextField.delegate  = self
-    
     }
     
-    
-    func validateTextField(label: UILabel, field: UITextField, validator: ValidatorFunction) {
-        if(!validator(field.text)) {
-            label.textColor = #colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)
-            field.becomeFirstResponder()
-        } else {
-            label.textColor = #colorLiteral(red: 0.1215686275, green: 0.1294117647, blue: 0.1411764706, alpha: 1)
-        }
-    }
-    
-    func focusLabel(_ label: UILabel) {
-        label.textColor = UIColor(named: "red")
-    }
-    
-
-}
-
-typealias ValidatorFunction = (_ text: String?) -> Bool
-
-extension AndroidHatersViewController : AndroidHatersDelegate {
-    func onSaved(haterInfo: String) {
-        
-    }
 }
 
 extension AndroidHatersViewController: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case nameTextField:
+            phoneNumberTextField.becomeFirstResponder()
+        case phoneNumberTextField:
+            emailAddressTextField.becomeFirstResponder()
+        case emailAddressTextField:
+            usernameTextField.becomeFirstResponder()
+        case usernameTextField:
+            passwordTextField.becomeFirstResponder()
+        case passwordTextField:
+            addButton.becomeFirstResponder()
+        case addButton:
+            nameTextField.becomeFirstResponder()
+        default:
+            self.becomeFirstResponder()
+        }
+        
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         
         switch textField {
-        case nameTextField: validateTextField(label: nameLabel, field: textField, validator: model.isValidName(text:))
-        case emailAddressTextFiled: validateTextField(label: emailAddressLabel, field: textField, validator: model.isValidEmailAddress(text:))
-        case phoneNumberTextField: validateTextField(label: phoneNumberLabel, field: textField, validator: model.isValidPhoneNumber(text:))
-        case usernameTextField: validateTextField(label: usernameLabel, field: textField, validator: model.isValidUsername(text:))
-        case passwordTextField: validateTextField(label: passwordLabel, field: textField, validator: model.isValidPassword(text:))
+        case nameTextField:
+            if AndroidHaterModel.isValidName(text: textField.text) {
+                model.name = textField.text
+                textField.textColor = UIColor.black
+                return true
+            }
+        case emailAddressTextField:
+            if AndroidHaterModel.isValidEmailAddress(text: textField.text) {
+                model.email = textField.text
+                textField.textColor = UIColor.black
+                return true
+            }
+        case phoneNumberTextField:
+            
+            
+            if AndroidHaterModel.isValidPhoneNumber(text: textField.text) {
+                model.phoneNumber = textField.text
+                textField.textColor = UIColor.black
+                return true
+            }
+        case usernameTextField:
+            if AndroidHaterModel.isValidUsername(text: textField.text) {
+                model.username = textField.text
+                textField.textColor = UIColor.black
+                return true
+            }
+        case passwordTextField:
+            if AndroidHaterModel.isValidPassword(text: textField.text) {
+                model.password = textField.text
+                textField.textColor = UIColor.black
+                return true
+            }
         default:
-            return
+            return true
         }
+        
+        textField.textColor = UIColor.red
+        return false
     }
 }
 
+
+extension AndroidHatersViewController : AndroidHatersDelegate {
+    func onSaved() {
+        textFields.forEach{$0.text = nil}
+        haterTextField.text = model.hatersAsString()
+    }
+    
+    func onCanSave(canSave: Bool) {
+        addButton.isEnabled = canSave
+    }
+}
